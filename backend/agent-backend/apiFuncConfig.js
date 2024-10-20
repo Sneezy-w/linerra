a71ea501b5984b9d2c8b8e5c14d8c2afe949d05b
Hamilton Wang <hamilton.y.wang@gmail.com>
2024-10-20 12:41:13 -0500

    serverless.yml

--- backend/agent-backend/apiFuncConfig.js
+++ backend/agent-backend/apiFuncConfig.js
@@ -8,20 +8,22 @@ module.exports = async ({ options, resolveVariable }) => {
   //console.log("stage", stage);
   //console.log("region", region);
 
-  const isOffline = !["prod", "test", "pre"].includes(options.stage);
+  //const isOffline = !["prod", "test", "pre"].includes(options.stage);
+  const isOffline = process.env?.npm_lifecycle_script?.includes(" offline") || false;
 
   //console.log("isOffline", options);
 
   //Resolver may return any JSON value (null, boolean, string, number, array or plain object)
   return {
     handler: "src/index.handler",
-    timeout: 300,
+    timeout: 120,
     url: {
       cors: {
         allowedOrigins: [frontendUrl],
         allowedHeaders: ["*"],
         allowedMethods: ["*"],
-      }
+      },
+      authorizer: 'aws_iam'
     },
     ...(isOffline && {
       events: [
